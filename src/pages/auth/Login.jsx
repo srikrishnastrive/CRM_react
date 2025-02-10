5c6939297c0ca6590940d4b2a5a6bb9d85a24d4b
srikrishnastrive <srikrishnamuddangala@gmail.com>
2025-02-10 10:39:07 +0530

    added the single page login page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,12 +1,27 @@
 function Login() {
     return (
-       <div>
+       <div className="flex justify-center items-center h-[90vh]">
             <div className="card bg-primary text-primary-content w-96">
-            <div className="card-body">
-                <h2 className="card-title">Card title!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                <button className="btn">Buy Now</button>
+            <div className="card-body flex items-center">
+                <div className="w-full flex justify-center mb-4">
+                    <h2 className="card-title text-4xl text-white ">Login</h2>
+                </div>
+                <div className="w-full">
+                    <input
+                        type="email"
+                        autoComplete="one-time-code"
+                        placeholder="User id "
+                        className="input input-bordered input-primary w-full max-w-xs text-white" />
+                </div>
+                <div className="w-full">
+                    <input
+                        type="password"
+                        autoComplete="one-time-code"
+                        placeholder="Password"
+                        className="input input-bordered input-primary w-full max-w-xs text-white " /> 
+                </div>
+                <div className="card-actions w-full mt-4">
+                <button className="btn btn-warning w-full font-bold hover:bg-accent-focus">Submit</button>
                 </div>
             </div>
             </div>
